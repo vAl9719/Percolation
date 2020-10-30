cfdeac087d05808b07be265a39b171ff5647cf57
Alyssa Zhao <alyssa.zhao@duke.edu>
2020-10-30 10:55:12 -0400

    percolationdfsfast tests passed?

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -1,5 +1,4 @@
 public class PercolationDFSFast extends PercolationDFS {
-
     /**
      * Initialize a grid so that all cells are blocked.
      *
@@ -7,5 +6,16 @@ public class PercolationDFSFast extends PercolationDFS {
      */
     public PercolationDFSFast(int n) {
         super(n);
+    }
+
+    @Override
+    protected void updateOnOpen(int row, int col) {
+        if(row == 0 ||
+                (inBounds(row-1, col) && isFull(row-1, col)) ||
+                (inBounds(row+1, col) && isFull(row+1, col)) ||
+                (inBounds(row, col-1) && isFull(row, col-1)) ||
+                (inBounds(row, col+1) && isFull(row, col+1))){
+            dfs(row, col);
+        }
     }
 }
--- src/TestPercolation.java
+++ src/TestPercolation.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.*;
 public class TestPercolation {
 	
 	public IPercolate getPercolator(int size) {
-		return new PercolationDFS(size);
+		//return new PercolationDFS(size);
 		//return new PercolationBFS(size);
-		//return new PercolationDFSFast(size);
+		return new PercolationDFSFast(size);
 		//IUnionFind finder = new QuickUWPC();
 		//IPercolate perc = new PercolationUF(finder,size);
 		//return perc;
