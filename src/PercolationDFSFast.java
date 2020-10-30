afbd9d3a3185f0a70444b6a34f165b4a477d32ea
Alyssa Zhao <alyssa.zhao@duke.edu>
2020-10-30 10:20:08 -0400

    added percolationdfsfast class

---
+++ src/PercolationDFSFast.java
@@ -0,0 +1,11 @@
+public class PercolationDFSFast extends PercolationDFS {
+
+    /**
+     * Initialize a grid so that all cells are blocked.
+     *
+     * @param n is the size of the simulated (square) grid
+     */
+    public PercolationDFSFast(int n) {
+        super(n);
+    }
+}
